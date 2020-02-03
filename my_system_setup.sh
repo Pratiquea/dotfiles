b1a2ca936fea61f20be7c81544075348b4314732
Prateek Arora <prateekarorav2@gmail.com>
2020-02-03 00:26:31 -0500

    added script for setting up a fresh system

---
+++ my_system_setup.sh
@@ -0,0 +1,21 @@
+############## vim ######
+#sudo apt -y install vim
+
+############## google chrome #######
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#sudo apt-get update 
+#sudo apt-get install google-chrome-stable
+
+############## vlc #######
+#sudo apt -y install vlc
+
+############## tlp  ##########
+#sudo apt -y install tlp
+
+############## gparted ####
+#sudo apt -y install gparted
+
+############## git #######
+sudo apt-get install git-all
+
