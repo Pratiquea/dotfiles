3c1351ebdb20fb2f503e6fe76d0aa73e97195541
Prateek Arora <prateekarorav2@gmail.com>
2020-02-03 20:00:54 -0500

    added a.vim and updated install script

--- my_system_setup.sh
+++ my_system_setup.sh
@@ -19,9 +19,24 @@
 ############## git #######
 #sudo apt-get install git-all
 
-############## flat mix #######
+############## flat-mix #######
 #sudo add-apt-repository ppa:daniruiz/flat-remix -y
 #sudo apt update
 #sudo apt -y install flat-remix-gtk
 
-############## flat mix #######
+############## sublime-text-3 #######
+#sudo apt install curl
+
+############## sublime-text-3 #######
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt -y install sublime-text
+
+############## Vundle #######
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+############## setup a.vim #######
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#cp a.vim ~/.vim/bundle/
+
